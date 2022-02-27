bc817e06c8a860accd97011c99c08a428f74dd1f
Ericgacoki <gacokieric@gmail.com>
2022-02-27 16:29:59 +0300

    Display signUpOrLogInScreen

--- app/src/main/java/com/ericg/neatflixdemo/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflixdemo/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ericg.neatflixdemo.screens.SignUpOrLogInScreen
-import com.ericg.neatflixdemo.screens.SplashScreen
+import com.ericg.neatflixdemo.screens.SignUpScreen
 import com.ericg.neatflixdemo.ui.theme.NeatFlixDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SignUpOrLogInScreen()
+                    SignUpScreen()
                 }
             }
         }
