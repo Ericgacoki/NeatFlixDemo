d374f8696a6b17e600814fba47f12c810d4cdfb6
Ericgacoki <gacokieric@gmail.com>
2022-03-05 12:20:51 +0300

    Display Home screen

--- app/src/main/java/com/ericg/neatflixdemo/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflixdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.ericg.neatflixdemo.screens.Home
 import com.ericg.neatflixdemo.screens.SignUpOrLogInScreen
 import com.ericg.neatflixdemo.screens.SignUpScreen
 import com.ericg.neatflixdemo.ui.theme.NeatFlixDemoTheme
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SignUpScreen()
+                    Home()
                 }
             }
         }
